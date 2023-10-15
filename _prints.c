075b1f83fe30dad2a232a11486fd258ef730cddb
MinaElka <aminakacimi1996@gmail.com>
2023-10-15 17:20:24 +0100

    add _prints.c file

---
+++ _prints.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _prints - prints a string
+ * @c: char to print
+ * Return: return the length of the str
+ */
+
+int	_prints(char *c)
+{
+	int i;
+
+	i = 0;
+	if (!c)
+		return (_prints("(null)"));
+	while (c[i])
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	return (i);
+}
