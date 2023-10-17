ea7fe4170ba4d77fa498534bfa4df700214b0e40
MinaElka <aminakacimi1996@gmail.com>
2023-10-17 01:41:44 +0100

    update

--- _printf.c
+++ _printf.c
@@ -1,33 +1,34 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
  * _printf - function that produces output according to a format
- * @format: caracter string containg specifier
+ * @format: caracter string
  * Return: number of caracter printed excluding null byte
  */
 
 int	_printf(const char *format, ...)
 {
-	va_list ptr;
-	int count = 0;
+	va_list	ptr;
+	int i;
+	int count;
 
+	count = 0;
+	i = 0;
 	va_start(ptr, format);
-	while (*format != '\0')
+	if (!format)
 	{
-		if (*format == '%' && (*(format + 1) == 'c'
-					|| *(format + 1) == 's' || *(format + 1) == '%'))
+		return (-1);
+	}
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			format++;
-			count += _check(ptr, *format);
+			i++;
+			count += _check(ptr, format[i]);
 		}
-		else
-		{
-			write(1, format, 1);
-			count++;
-		}
-		format++;
+		else if (format[i] != '\0')
+			count += _putchar(format[i]);
+		i++;
 	}
 	va_end(ptr);
 	return (count);
