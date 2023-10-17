d0b8cea44edf2fdb7049f3116d650f187736e303
MinaElka <aminakacimi1996@gmail.com>
2023-10-17 01:35:22 +0100

    update _check.c

--- _check.c
+++ _check.c
@@ -1,45 +1,53 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
  * _check - calls the appropriate prints functions based on the specifier
  * @args: variable argument list
  * @c: format specifier
+ * @count: keep truck
  * Return: number of caracter printed excluding null byte
  */
 
-int _check(va_list args, char c)
+int _check(va_list args, char c, int *count)
 {
-	int count;
+	int length = 0;
 
-	count = 0;
 	if (c == 'c')
-		count += _printc(va_arg(args, int));
+		length += _printc(va_arg(args, int), count);
 	else if (c == 's')
-		count += _prints(va_arg(args, char *));
+		length += _prints(va_arg(args, char *), count);
 	else if (c == 'p')
 	{
-		count += write(1, "0x", 2);
-		_printx(va_arg(args, unsigned long), 'x', &count);
+		*count += write(1, "0x", 2)
+		length += _printx(va_arg(args, unsigned long), 'x', count);
 	}
 	else if (c == 'd' || c == 'i')
-		_printd(va_arg(args, int), &count);
+		length += _printd(va_arg(args, int), count);
 	else if (c == 'u')
-		_printu(va_arg(args, unsigned int), &count);
+		length += _printu(va_arg(args, unsigned int), count);
 	else if (c == 'x' || c == 'X')
-		_printx(va_arg(args, unsigned int), c, &count);
+		length += _printx(va_arg(args, unsigned int), c, count);
 	else if (c == '%')
-		count += write(1, "%", 1);
+	{
+		*count += write(1, "%", 1);
+		length += 1;
+	}
 	else if (c == 'b')
-		_printb(va_arg(args, unsigned int), &count);
+		length += _printb(va_arg(args, unsigned int), count);
 	else if (c == 'o')
-		_printo((unsigned long)va_arg(args, unsigned long), &count);
+		length += _printo((unsigned long)va_arg(args, unsigned long), count);
 	else if (c == 'S')
-		count += _printss(va_arg(args, char *));
+		length += _printss(va_arg(args, char *), count);
 	else if (c == 'r')
-		_printr(va_arg(args, char *), &count);
+		length += _printr(va_arg(args, char *), count);
 	else if (c == 'R')
-		_print_rot_13(va_arg(args, char *), &count);
+		length += _print_rot_13(va_arg(args, char *), count);
 	else
-		count += _putchar(c);
-	return (count);
+	{
+		*count += _putchar(c);
+		length += 1;
+	}
+	return (length);
 }
